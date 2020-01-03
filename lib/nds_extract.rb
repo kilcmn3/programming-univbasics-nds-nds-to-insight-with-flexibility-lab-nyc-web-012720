34c062495d254345c232777900d1a0ed025e3588
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-03 23:21:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,6 +50,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   result = movies_with_director_names(name, movies_collection)
+  puts result
 end
 
 
