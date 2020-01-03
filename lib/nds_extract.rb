814ef14f44c2d31f12c8b8fe76b784d2c67a9b78
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-03 23:48:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,11 +56,8 @@ def movies_with_director_key(name, movies_collection)
     result << movie_with_director_name(name, movies_collection[i])
     i += 1
   end
-
-
-
-
-
+  pp result
+  result
 end
 
 
