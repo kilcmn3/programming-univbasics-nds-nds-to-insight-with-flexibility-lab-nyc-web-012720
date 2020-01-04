bb400e4285bd636715aa8d988735b54a9041ef23
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-04 00:59:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -100,10 +100,10 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  new_array = []
-  [:name => "#{source[0][:name]}", :movies =>[
-    {:title => "#{source[0][:movies][:title]}"}
-    ]]
+  # new_array = []
+  # [:name => "#{source[0][:name]}", :movies =>[
+  #   {:title => "#{source[0][:movies][:title]}"}
+  #   ]]
     pp source[0][:movies][:title]
 end
 
